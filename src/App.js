ca29590ee8dbe70307a71039ee946e630fdaa8c4
Guruprasad Raikar <gp.raikar@planview.com>
2025-03-19 19:07:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 GitOps is Working!</h1>
+      <p>This message updates automatically after every Git push.</p>
+    </div>
+  );
+}
+
+export default App;

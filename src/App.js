591fdfa411bb6605ca1d0cf447fbec12e8310a65
Guruprasad Raikar <gp.raikar@planview.com>
2025-03-19 19:30:18 +0530

    Updated GitOps message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <h1>🚀 GitOps is Working!</h1>
       <p>This message updates automatically after every Git push.</p>
+
+      <h1>🚀 GitOps is Awesome!</h1>
+      <p>This change is deployed automatically.</p>
+
     </div>
   );
 }
